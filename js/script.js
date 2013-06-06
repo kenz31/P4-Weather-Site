9c28db4a89337d93919fb8109fb8547179f4545a
Mckenzie Murphy <mckenzie.murphy9@gmail.com>
2013-06-06 10:53:58 -0700

    finished prototype

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  
+/*  $('button').click(function(){
+	  
+	  var zip = $('input').val();
+  });*/
+  
+	  //makes weather
+ $.simpleWeather({
+	zipcode: '',
+	woeid: '2357536',
+	location: '',
+	unit: 'f',
+	success: function(weather) {
+		html = '<h2>'+weather.city+', '+weather.region+'</h2>';
+		html += '<img style="float:left;" width="125px" src="'+weather.image+'">';
+		html += '<p>'+weather.temp+'&deg; '+weather.units.temp+'<br /><span>'+weather.currently+'</span></p>';
+		html += '<a href="'+weather.link+'">View Forecast &raquo;</a>';
+ 
+		$("#weather").html(html);
+	},
+	error: function(error) {
+		$("#weather").html('<p>'+error+'</p>');
+	}
+});
+});
